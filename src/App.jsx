ddfa3ba805cdcff17943568102c8bca39b542235
Oliver Doan <doanvietthuan1005@gmail.com>
2022-08-04 14:12:01 +0700

    UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import './assets/scss/styles.scss'
+
+import { Typography, Divider } from 'antd'
+import TodoList from './components/TodoList'
+import Filters from './components/Filters'
+
+const { Title } = Typography
+
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div
+      style={{
+        width: 500,
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        padding: 20,
+        boxShadow: '0 0 10px 4px #bfbfbf',
+        borderRadius: 5,
+        height: '90vh',
+      }}
+    >
+      <Title style={{ textAlign: 'center' }}>TODO APP with REDUX</Title>
+      <Filters />
+      <Divider />
+      <TodoList />
+    </div>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
+      { find: /^~/, replacement: '' },
       { find: '@', replacement: path.resolve(__dirname, './src') },
       {
         find: '@components',
