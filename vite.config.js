a01ed2eb7a7b11b6e9cab83436064e5c118116f3
OliverDoan <doanvietthuan1005@gmail.com>
2022-08-13 23:17:35 +0700

    add ui

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
+      { find: /^~/, replacement: '' },
       { find: '@', replacement: path.resolve(__dirname, './src') },
       {
         find: '@components',
         replacement: path.resolve(__dirname, './src/components'),
       },
-      { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, './src/assets'),
+      },
     ],
   },
 })
