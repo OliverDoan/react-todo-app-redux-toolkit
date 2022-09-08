f94fcb9a8b5a3b9e5e421d04a85e23cd90e03e4a
OliverDoan <doanvietthuan1005@gmail.com>
2022-09-08 23:18:54 +0700

    delete all

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,36 +6,48 @@ export const addTodo = (data = {}) => {
     payload: data,
   }
 }
+
 export const deleteTodo = (id = '') => {
   return {
     type: 'todoList/deleteTodo',
     payload: id,
   }
 }
+
+export const deleteAllTodo = () => {
+  return {
+    type: 'todoList/deleteAllTodo',
+  }
+}
+
 export const updateTodo = (data = {}) => {
   return {
     type: 'todoList/updateTodo',
     payload: data,
   }
 }
+
 export const toggleTodoStatus = (todoId) => {
   return {
     type: 'todoList/toggleTodoStatus',
     payload: todoId,
   }
 }
+
 export const searchFilterChange = (text = '') => {
   return {
     type: 'filters/searchFilterChange',
     payload: text,
   }
 }
+
 export const statusFilterChange = (status = '') => {
   return {
     type: 'filters/statusFilterChange',
     payload: status,
   }
 }
+
 export const priorityFilterChange = (priorities = []) => {
   return {
     type: 'filters/prioritiesFilterChange',
